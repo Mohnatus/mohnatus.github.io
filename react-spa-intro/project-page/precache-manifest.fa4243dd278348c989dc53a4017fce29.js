0ab02fc78a2788e10d09e422fe858f0121d57812
FurryCat <furrycat.web@gmail.com>
2020-01-12 17:55:35 +0300

    react-spa-intro 4

---
+++ react-spa-intro/project-page/precache-manifest.fa4243dd278348c989dc53a4017fce29.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a234b76eb88de9f8c59e",
+    "url": "../static/js/2.971bce19.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "../static/js/2.971bce19.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4ee32fa9bf1c7ee1902d",
+    "url": "../static/js/main.f8eea4fe.chunk.js"
+  },
+  {
+    "revision": "17ff1ac5692c2ca16309c777680c267b",
+    "url": "./static/media/react.17ff1ac5.jpeg"
+  },
+  {
+    "revision": "e27743b4bd71f75e6be875588ad87476",
+    "url": "./static/media/spa-1.e27743b4.jpg"
+  },
+  {
+    "revision": "ca02568c4cb6863d422454896e0c059d",
+    "url": "./static/media/spa-2.ca02568c.jpg"
+  },
+  {
+    "revision": "5f695f56b0a30b1eb44d18934efff609",
+    "url": "./static/media/spa-3.5f695f56.jpg"
+  },
+  {
+    "revision": "dcdf0c16ac69b8a6fbe29ced7462defe",
+    "url": "./static/media/spa-4.dcdf0c16.jpg"
+  },
+  {
+    "revision": "1d3867f9d2a8dba158e8a11a18ee495b",
+    "url": "./static/media/spa-5.1d3867f9.jpg"
+  },
+  {
+    "revision": "c17060ab2913558c7863423a6d5be391",
+    "url": "./static/media/spa-6.c17060ab.jpg"
+  },
+  {
+    "revision": "f025aacfb28efdf80b275a89d18f6891",
+    "url": ".index.html"
+  },
+  {
+    "revision": "4ee32fa9bf1c7ee1902d",
+    "url": ".static/css/main.04dd7139.chunk.css"
+  },
+  {
+    "revision": "2395ed3c609a288eb0fc",
+    "url": ".static/js/runtime-main.db10e449.js"
+  }
+]);
