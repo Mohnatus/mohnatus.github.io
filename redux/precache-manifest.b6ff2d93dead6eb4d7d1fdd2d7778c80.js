2ca3ddc860f84d4a4bf916394a227c40d1bedeae
FurryCat <furrycat.web@gmail.com>
2020-03-11 16:56:38 +0300

    redux course

---
+++ redux/precache-manifest.b6ff2d93dead6eb4d7d1fdd2d7778c80.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3df0f4bd815576c068a2e7637b86e63d",
+    "url": "https://mohnatus.github.io/redux/index.html"
+  },
+  {
+    "revision": "57ccb3773a8056bf4a77",
+    "url": "https://mohnatus.github.io/redux/static/css/main.3602fca1.chunk.css"
+  },
+  {
+    "revision": "fa8b2cda9ebfa8d94316",
+    "url": "https://mohnatus.github.io/redux/static/js/2.09136b46.chunk.js"
+  },
+  {
+    "revision": "94b29ac6d0ec426932e82da99c88b81c",
+    "url": "https://mohnatus.github.io/redux/static/js/2.09136b46.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57ccb3773a8056bf4a77",
+    "url": "https://mohnatus.github.io/redux/static/js/main.1bc0f2f1.chunk.js"
+  },
+  {
+    "revision": "aa6a50b17e35efbb7877",
+    "url": "https://mohnatus.github.io/redux/static/js/runtime-main.c4418253.js"
+  }
+]);
