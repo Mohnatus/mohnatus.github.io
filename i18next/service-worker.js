279bc3cf41ac7db0bbb128905e856297c70e5d09
mohnatus <furrycat.web@gmail.com>
2020-10-10 16:09:28 +0300

    i18next

--- i18next/service-worker.js
+++ i18next/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ee5a451bfc0f0ea8d209aefb40f63cce.js"
+  "https://mohnatus.github.io/i18next/precache-manifest.ddf95f32f2de760f1fe38faf978a9ea3.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://mohnatus.github.io/i18next/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
